3648f60682313047ab2d2ac45b454e5efde073bb
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-06-12 23:26:58 -0500

    feat: :sparkles: Las consultas de clase fueron completadas

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import { 
     getAllLastNameASC, 
-    getAllLastNameDESC
+    getAllLastNameDESC,
+    getCustomerInfoById,
+    getCustomersInNYC,
+    getCustomerSalesReps
 } from "./js/module/customers.js";
 
 
@@ -8,8 +11,56 @@ import {
 import { getAll, 
     getAllLastName, 
     getAllFullNameJob, 
-    getAllJobTitle 
+    getAllJobTitle,
+    getEmployeesByOfficeCode,
+    getEmployeeBossPairs,
+    getEmployeeOfficeInfo
 } from "./js/module/employees.js";
 
+import {
+    getAllProductInfo,
+    getTotalStockQuantity,
+    getProductsAbovePrice,
+    getProductTotalOrdered
+} from "./js/module/products.js";
 
-console.log(await getAllLastNameDESC());
+
+
+import{
+    getPaymentsByCustomerNumber
+} from "./js/module/payments.js";
+
+
+import{
+    getShippedOrders,
+    getOrderAndCustomerInfo
+} from "./js/module/orders.js";
+
+
+import {
+    getOfficesInJapan,
+    getOfficeEmployeeCount
+} from "./js/module/offices.js";
+
+
+// console.log(await getAllLastNameDESC());
+
+// -----------------------------------------------------------------------
+
+// Consultas de la actividad en clase
+
+// console.log(await getAllProductInfo());
+// console.log(await getEmployeesByOfficeCode());
+// console.log(await getCustomerInfoById());
+// console.log(await getPaymentsByCustomerNumber());
+// console.log(await getShippedOrders());
+// console.log(await getTotalStockQuantity());
+// console.log(await getEmployeeBossPairs());
+// console.log(await getOfficesInJapan());
+// console.log(await getCustomersInNYC());
+// console.log(await getProductsAbovePrice());
+// console.log(await getOrderAndCustomerInfo());
+// console.log(await getEmployeeOfficeInfo());
+// console.log(await getCustomerSalesReps());
+// console.log(await getProductTotalOrdered());
+console.log(await getOfficeEmployeeCount());
